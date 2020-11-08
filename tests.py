487c56b482fd24736609b64aa04606071fd00cb9
carter-le <61708542+carter-le@users.noreply.github.com>
2020-11-08 12:27:46 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 from csvData import csvData 
 
-
 c = csvData('wifi_list.csv')
 print(c.originalFields)
+print(c.totalRows)
+print(c.mismatchRows)
+
+#c = csvData('wifi_list.csv')
+#print(c.originalFields)
 print(c.filteredFields)
 c.guessTypes()
 
